576e4a67488c70de1677d540d7e1accdfc7a0668
kyleeardley <kyleeardley@gmail.com>
2013-09-04 21:21:22 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EardleyConstruction::Application.config.secret_key_base = 'b3debd71e566cf5e7c9fc67a8f1c7a16ab408aa84476c9898400549887290130b1c74e33f2384161a6ac569c0b92f1b96c82961ef0d8640b371d9c3fd6d5726d'
